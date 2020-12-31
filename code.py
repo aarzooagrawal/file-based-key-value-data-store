de6ab35ae6d797f261db89a85354d1f503e81d33
Aarzoo Agrawal <68186732+aarzooagrawal@users.noreply.github.com>
2020-12-31 14:45:51 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,5 +1,4 @@
 import threading 
-#this is for python 3.0 and above. use import thread for python2.0 versions
 from threading import*
 import time
 
@@ -10,35 +9,35 @@ d={} #'d' is the dictionary in which we store data
 
 def create(key,value,timeout=0):
     if key in d:
-        print("error: this key already exists") #error message1
+        print("error: this key already exists")
     else:
         if(key.isalpha()):
-            if len(d)<(1024*1020*1024) and value<=(16*1024*1024): #constraints for file size less than 1GB and Jasonobject value less than 16KB 
+            if len(d)<(1024*1020*1024) and value<=(16*1024*1024):  
                 if timeout==0:
                     l=[value,timeout]
                 else:
                     l=[value,time.time()+timeout]
-                if len(key)<=32: #constraints for input key_name capped at 32chars
+                if len(key)<=32: 
                     d[key]=l
             else:
-                print("error: Memory limit exceeded!! ")#error message2
+                print("error: Memory limit exceeded!! ")
         else:
-            print("error: Invalind key_name!! key_name must contain only alphabets and no special characters or numbers")#error message3
+            print("error: Invalind key_name!! key_name must contain only alphabets and no special characters or numbers")
 
 #for read operation
 #use syntax "read(key_name)"
             
 def read(key):
     if key not in d:
-        print("error: given key does not exist in database. Please enter a valid key") #error message4
+        print("error: given key does not exist in database. Please enter a valid key") 
     else:
         b=d[key]
         if b[1]!=0:
-            if time.time()<b[1]: #comparing the present time with expiry time
-                stri=str(key)+":"+str(b[0]) #to return the value in the format of JasonObject i.e.,"key_name:value"
+            if time.time()<b[1]:
+                stri=str(key)+":"+str(b[0])
                 return stri
             else:
-                print("error: time-to-live of",key,"has expired") #error message5
+                print("error: time-to-live of",key,"has expired") 
         else:
             stri=str(key)+":"+str(b[0])
             return stri
@@ -48,20 +47,18 @@ def read(key):
 
 def delete(key):
     if key not in d:
-        print("error: given key does not exist in database. Please enter a valid key") #error message4
+        print("error: given key does not exist in database. Please enter a valid key") 
     else:
         b=d[key]
         if b[1]!=0:
-            if time.time()<b[1]: #comparing the current time with expiry time
+            if time.time()<b[1]:
                 del d[key]
                 print("key is successfully deleted")
             else:
-                print("error: time-to-live of",key,"has expired") #error message5
+                print("error: time-to-live of",key,"has expired") 
         else:
             del d[key]
             print("key is successfully deleted")
-
-#I have an additional operation of modify in order to change the value of key before its expiry time if provided
 
 #for modify operation 
 #use syntax "modify(key_name,new_value)"
@@ -71,17 +68,17 @@ def modify(key,value):
     if b[1]!=0:
         if time.time()<b[1]:
             if key not in d:
-                print("error: given key does not exist in database. Please enter a valid key") #error message6
+                print("error: given key does not exist in database. Please enter a valid key") 
             else:
                 l=[]
                 l.append(value)
                 l.append(b[1])
                 d[key]=l
         else:
-            print("error: time-to-live of",key,"has expired") #error message5
+            print("error: time-to-live of",key,"has expired") 
     else:
         if key not in d:
-            print("error: given key does not exist in database. Please enter a valid key") #error message6
+            print("error: given key does not exist in database. Please enter a valid key") 
         else:
             l=[]
             l.append(value)
